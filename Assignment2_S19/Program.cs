1d0d9be897fc5f86244299318d17a95c56640513
Janvi Vora <janvi@usf.edu>
2019-02-08 15:14:16 -0500

    Q1,Q2 and sorting

--- Assignment2_S19/Program.cs
+++ Assignment2_S19/Program.cs
@@ -54,7 +54,7 @@ namespace Assignment2_S19
             int year = 2017;
             Console.WriteLine(dayOfProgrammer(year));
         }
-
+        /* This function will display the array */
         static void displayArray(int []arr) {
             Console.WriteLine();
             foreach(int n in arr) {
@@ -62,17 +62,73 @@ namespace Assignment2_S19
             }
         }
 
-        // Complete the rotLeft function below.
+        /* This function will perform the given left rotation on the array */
         static int[] rotLeft(int[] a, int d)
         {
-            return new int[] {};
+            // outer loop to shift the elements to left d times.
+            for (int j = 0; j < d; j++)
+            {
+                // Take the first element and store it safely in a temporary variable.
+                // It will be later assigned to the last element in the array
+                int temp = a[0];
+                int arrayLength = a.Length;
+                // Inner loop to update the array by shifting all the elements to left by 1 bit
+                for (int i = 1; i < arrayLength; i++)
+                {
+                    a[i - 1] = a[i];
+                    // Assign the first element to the last element of the updated array
+                    if (i == arrayLength - 1)
+                    {
+                        a[i] = temp;
+                    }
+                }
+
+            }
+            return a;
         }
 
-        // Complete the maximumToys function below.
+        /* This function will find the maximum number of toys in a fixed budget */
         static int maximumToys(int[] prices, int k)
         {
-            return 0;
+            int[] sortedPrices = sortAscArray(prices);
+            int itemCount = 0;
+            int totalExpense = 0;
+            for (int i = 0; i < sortedPrices.Length; i++)
+            {
+                totalExpense = totalExpense + sortedPrices[i];
+                // If the total expense exceeds the budget, exit the loop
+                if (totalExpense > k)
+                {
+                    break;
+                }
+                itemCount++;
+            }
+            return itemCount;
         }
+
+        /* This function will sort the 1-D integer array in ascending order using selection sort algorithm */
+        static int[] sortAscArray(int[] arr)
+        {
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+
+                int min_idx = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[min_idx])
+                        min_idx = j;
+                }
+                // Swap the found minimum element with the first
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+
+            }
+            return arr;
+        }
+
+
 
         // Complete the balancedSums function below.
         static string balancedSums(List<int> arr)
